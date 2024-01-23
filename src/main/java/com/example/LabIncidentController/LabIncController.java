2eb31236a31cde255eb9ebefa387c41a670178cc
Shivam Patel <shivaapatel2805@gmail.com>
2024-01-24 00:00:50 +0530

    change

--- src/main/java/com/example/LabIncidentController/LabIncController.java
+++ src/main/java/com/example/LabIncidentController/LabIncController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("http://Localhost:5173")
 @RequestMapping("/LabIncident/api")
 public class LabIncController {
     @Autowired
